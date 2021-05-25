0dcf08b89e4f079a6a03b1ae555db4c4afdf873b
joao <joao.nilton@gmail.com>
2021-05-25 19:17:13 -0300

    250521

---
+++ olamundo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <title>   </title>
+    
+    </head>
+    <body>
+        <?php
+            echo "Olá, Mundo";
+            phpinfo();
+            echo"<h1>Olá Mundo!</h1>";
+        ?>
+
+    </body>
+
+</html>
---
+++ olamundoI.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1> Olá Mundo! </h1>"; 
+
+?>
+
